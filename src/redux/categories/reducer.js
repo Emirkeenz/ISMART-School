b795682abd57ea9ad824bffd2d610c496bdf263e
Emir <admin@MacBook-Air--Emir.local>
2024-05-09 16:46:27 +0600

    fixed redux and rendered categories with subcategories

--- src/redux/categories/reducer.js
+++ src/redux/categories/reducer.js
@@ -1,19 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import api from '../../service/api/api';
 
-export const fetchCategoriesList = createAsyncThunk('category/fetchCategoriesList', async () => {
+export const getCategoriesList = createAsyncThunk('category/getCategoriesList', async () => {
   try {
     const response = await api.categories.getCategoriesList();
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-});
-
-export const fetchCategory = createAsyncThunk('category/fetchCategory', async (id) => {
-  try {
-    const response = await api.categories.getCategory(id);
+    console.log(response);
     return response.data;
   } catch (error) {
     console.error(error);
